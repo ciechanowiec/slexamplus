845a2cc8f3e0361132ed598f50a7e5ea005007de
Herman Ciechanowiec <herman@ciechanowiec.eu>
2025-10-12 12:15:29 +0200

    Refine calendar initialization

--- bundle/src/main/java/eu/ciechanowiec/slexamplus/CalendarInitialization.java
+++ bundle/src/main/java/eu/ciechanowiec/slexamplus/CalendarInitialization.java
@@ -4,6 +4,7 @@ import eu.ciechanowiec.sling.rocket.calendar.StagedCalendarNode;
 import eu.ciechanowiec.sling.rocket.commons.FullResourceAccess;
 import eu.ciechanowiec.sling.rocket.jcr.NodeProperties;
 import eu.ciechanowiec.sling.rocket.jcr.path.TargetJCRPath;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -11,22 +12,35 @@ import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceCardinality;
 
 import java.time.Year;
+import java.util.Optional;
 
 @Component(
     service = CalendarInitialization.class,
     immediate = true
 )
+@Slf4j
 public class CalendarInitialization {
 
-    @SuppressWarnings("MagicNumber")
+    @SuppressWarnings({"MagicNumber", "squid:S1075"})
     @Activate
     public CalendarInitialization(
         @Reference(cardinality = ReferenceCardinality.MANDATORY)
         FullResourceAccess fullResourceAccess
     ) {
-        TargetJCRPath calendarPath = new TargetJCRPath("/content/calendar");
-        new StagedCalendarNode(Year.of(2024), Year.of(2025), fullResourceAccess).save(calendarPath);
-        NodeProperties nodeProperties = new NodeProperties(calendarPath, fullResourceAccess);
-        nodeProperties.setProperty(ResourceResolver.PROPERTY_RESOURCE_TYPE, "slexamplus/application/calendar");
+        try (ResourceResolver resourceResolver = fullResourceAccess.acquireAccess()) {
+            String calendarPathRaw = "/content/calendar";
+            Optional.ofNullable(resourceResolver.getResource(calendarPathRaw))
+                .ifPresentOrElse(
+                    resource -> log.info("Calendar already exists ({}), skipping initialization", resource),
+                    () -> {
+                        TargetJCRPath calendarPath = new TargetJCRPath(calendarPathRaw);
+                        new StagedCalendarNode(Year.of(2024), Year.of(2025), fullResourceAccess).save(calendarPath);
+                        NodeProperties nodeProperties = new NodeProperties(calendarPath, fullResourceAccess);
+                        nodeProperties.setProperty(
+                            ResourceResolver.PROPERTY_RESOURCE_TYPE, "slexamplus/application/calendar"
+                        );
+                    }
+                );
+        }
     }
 }
